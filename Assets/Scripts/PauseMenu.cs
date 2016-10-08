6622ca4715e639ebcf26decc7b665c392bad5ab8
qihanghu <qihang_hu@utexas.edu>
2016-10-08 14:55:52 -0500

    Added a pause menu to Sprint 1 Level

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    private bool panelDisplayed = false;
+    public GameObject pausePanel;
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape) && !panelDisplayed){
+            ShowPanel();
+        } else if (Input.GetKeyDown(KeyCode.Escape) && panelDisplayed){
+            HidePanel();
+        } 
+    }
+
+    private void ShowPanel(){
+        panelDisplayed = true;
+        pausePanel.SetActive(panelDisplayed);
+        Time.timeScale = 0;
+    }
+
+    private void HidePanel(){
+        panelDisplayed = false;
+        pausePanel.SetActive(panelDisplayed);
+        Time.timeScale = 1;
+    }
+
+    public void ResumeGame(){
+        panelDisplayed = false;
+        pausePanel.SetActive(panelDisplayed);
+        Time.timeScale = 1;
+    }
+
+    public void RestartGame(){
+        Application.LoadLevel("Sprint 1 Level");
+        Time.timeScale = 1;
+    }
+
+    public void ExitGame(){
+        Application.LoadLevel("UI Test");
+    }
+}
