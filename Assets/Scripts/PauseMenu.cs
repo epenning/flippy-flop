ba1877b9c2e49aba895d100a1f8c3e28821fdad2
qihanghu <qihang_hu@utexas.edu>
2016-10-21 18:10:12 -0500

    Added keyboard control to the pause menu, and added "press any key to play" to the inner-game menu.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,40 +3,103 @@ using System.Collections;
 
 public class PauseMenu : MonoBehaviour {
 
+    //Initiate the pasue menu as invisable
     private bool panelDisplayed = false;
     public GameObject pausePanel;
 
+    //Define the amount of buttons displayed on screen
+    public int buttonsCount = 3;
+
+    //Define the button selector
+    public int selector;
+
+    //Define the number of button
+    public int ID;
+
     void Update(){
-        if (Input.GetKeyDown(KeyCode.Escape) && !panelDisplayed){
+        //Display or hide the pause menu based on keyboard input
+        if (Input.GetKeyDown(KeyCode.Escape) && !panelDisplayed)
+        {
+            Time.timeScale = 0;
             ShowPanel();
         } else if (Input.GetKeyDown(KeyCode.Escape) && panelDisplayed){
             HidePanel();
-        } 
+        }
+         
     }
 
+    //Display the pause menu
     private void ShowPanel(){
         panelDisplayed = true;
         pausePanel.SetActive(panelDisplayed);
-        Time.timeScale = 0;
+
+        //Assign the vaule to selector based on keyboard input
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //Restric the minimun value of the selector
+            if (selector < buttonsCount)
+            {
+                selector += 1;
+            }
+            else
+            {
+                selector = 1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //Restric the maxmun value of teh selector
+            if (selector > buttonsCount)
+            {
+                selector -= 1;
+            }
+            else
+            {
+                selector = buttonsCount;
+            }
+        }
+
+        //Change scenes based on the button selected
+        if (Input.GetKeyDown(KeyCode.Return) && ID == selector)
+        {
+            switch (selector)
+            {
+                case 1:
+                    ResumeGame();
+                    break;
+
+                case 2:
+                    RestartGame();
+                    break;
+
+                case 3:
+                    ExitGame();
+                    break;
+            }
+        }
     }
 
+    //Hide the pause menu
     private void HidePanel(){
         panelDisplayed = false;
         pausePanel.SetActive(panelDisplayed);
         Time.timeScale = 1;
     }
 
+    //Resume function
     public void ResumeGame(){
         panelDisplayed = false;
         pausePanel.SetActive(panelDisplayed);
         Time.timeScale = 1;
     }
 
+    //Restart function
     public void RestartGame(){
         Application.LoadLevel("Sprint 1 Level");
         Time.timeScale = 1;
     }
 
+    //Exit function
     public void ExitGame(){
         Application.LoadLevel("UI Test");
         Time.timeScale = 1;
---
+++ Assets/Scripts/PressAnyKey.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PressAnyKey : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+            SceneManager.LoadScene(1);
+    }
+}
