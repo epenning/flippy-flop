f8e564a4259f9e456e6443f75a90b24d7c5e5974
Erin Pennington <epennington@utexas.edu>
2016-10-28 12:12:31 -0500

    set up gamekid intro with random test mp4, made main menu change levels to settings and credits by name instead of number

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayVideo : MonoBehaviour {
+
+    public MovieTexture movie;
+    
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        StartCoroutine(playIntroMusic());
+	}
+
+    IEnumerator playIntroMusic()
+    {
+        movie.Play();
+        yield return new WaitForSeconds(movie.duration);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
