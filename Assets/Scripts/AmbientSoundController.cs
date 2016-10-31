e0e0325d142a07bb5164828942ed8b8085b3c29c
r-escobar <rescobar713@gmail.com>
2016-10-30 22:30:17 -0500

    Revert "Added ambient sound controller to flag sounds to be played on a certain side"
    This reverts commit d769f6aa9a31b0b7057c7ceef883118d8da67b6f.

--- Assets/Scripts/AmbientSoundController.cs
+++ Assets/Scripts/AmbientSoundController.cs
@@ -3,12 +3,9 @@ using System.Collections;
 
 public class AmbientSoundController : MonoBehaviour {
 
-    public bool persistThroughLevels;
-
 	// Use this for initialization
 	void Start () {
-        if(persistThroughLevels)
-            DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 	
 	// Update is called once per frame
