dc1e05f813158b2b1756c2dfd89abf4a479a3aaf
r-escobar <rescobar713@gmail.com>
2016-11-02 23:54:50 -0500

    Added a temporary looping reflection video

---
+++ Assets/Scripts/LoopingVideo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoopingVideo : MonoBehaviour
+{
+
+    public MovieTexture movie;
+
+    void Start()
+    {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        movie.loop = true;
+        movie.Play();
+    }
+}
