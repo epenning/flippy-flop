9c0b14a4b08b08abbf6622df46ddaa11f7ae246b
r-escobar <rescobar713@gmail.com>
2016-12-05 16:46:21 -0600

    Added particle effects to dangerous objects

---
+++ Assets/Scripts/DangerParticleController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DangerParticleController : MonoBehaviour {
+
+    public bool active;
+
+    ParticleSystem partSys;
+
+    LevelController levelScript;
+
+	// Use this for initialization
+	void Awake () {
+        levelScript = GameObject.Find("LevelController").GetComponent<LevelController>();
+        partSys = GetComponent<ParticleSystem>();
+        partSys.GetComponent<Renderer>().sortingOrder = -70;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(levelScript.currDir == 1)
+        {
+            active = true;
+        } else
+        {
+            active = false;
+        }
+
+
+        if(partSys.isPlaying)
+        {
+            if(!active)
+            {
+                partSys.Stop();
+            }
+        } else
+        {
+            if(active)
+            {
+                partSys.Play();
+            }
+        }
+    }
+}
