94b022ca8b7ce420158cc7b7d4469e24ba15f8c0
Erin Pennington <epennington10@gmail.com>
2016-12-09 13:47:53 -0600

    fixed music volume problems in level 3

--- Assets/Scripts/Level4MusicVolFade.cs
+++ Assets/Scripts/Level4MusicVolFade.cs
@@ -6,15 +6,21 @@ public class Level4MusicVolFade : MonoBehaviour {
     VolumeFadeOut volAdjuster;
     MasterVolumeController masterVolScript;
 
+    public float musicVolMax;
+    public float footstepVolMax;
+
 	// Use this for initialization
 	void Start () {
         volAdjuster = GameObject.Find("Volume Adjuster").GetComponent<VolumeFadeOut>();
         masterVolScript = GameObject.Find("MasterVolumeController").GetComponent<MasterVolumeController>();
+
+        musicVolMax = masterVolScript.musicVol;
+        footstepVolMax = masterVolScript.footstepVol;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        masterVolScript.musicVol = volAdjuster.currVol;
-        masterVolScript.footstepVol = volAdjuster.currVol;
+        masterVolScript.musicVol = volAdjuster.currVol * musicVolMax;
+        masterVolScript.footstepVol = volAdjuster.currVol * footstepVolMax;
     }
 }
